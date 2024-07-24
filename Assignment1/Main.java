d91cfc387adbf2a295d4d00cc643eba3984a92d0
Kaviyadharsan <131509471+Kaviyadharsan@users.noreply.github.com>
2024-07-24 16:20:46 +0530

    Add files via upload

---
+++ Assignment1/Main.java
@@ -0,0 +1,15 @@
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Main class is executing.");
+        Sub.sub(); 
+    }
+}
+
+class Sub {
+    static void sub() {
+        System.out.println("Sub class is executing.");
+        
+    }
+}
+
