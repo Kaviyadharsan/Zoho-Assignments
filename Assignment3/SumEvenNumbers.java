f57d89dcb59d60b6cda14e264b17d5d388085553
Kaviyadharsan <131509471+Kaviyadharsan@users.noreply.github.com>
2024-07-27 15:23:04 +0530

    Add files via upload

---
+++ Assignment3/SumEvenNumbers.java
@@ -0,0 +1,21 @@
+package assignment2;
+
+public class SumEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                continue;
+            }
+            sum += number; 
+        }
+
+        System.out.println("The sum of even numbers in the array is: " + sum);
+	}
+
+}
+
