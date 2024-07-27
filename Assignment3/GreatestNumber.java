226cce14d172cf52d7954de82f068805efd784ed
Kaviyadharsan <131509471+Kaviyadharsan@users.noreply.github.com>
2024-07-27 13:17:55 +0530

    Add files via upload

---
+++ Assignment3/GreatestNumber.java
@@ -0,0 +1,45 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Scanner scanner = new Scanner(System.in);
+
+		        
+		        System.out.print("Enter the first number: ");
+		        int num1 = scanner.nextInt();
+
+		        System.out.print("Enter the second number: ");
+		        int num2 = scanner.nextInt();
+
+		        System.out.print("Enter the third number: ");
+		        int num3 = scanner.nextInt();
+
+		   
+		        scanner.close();
+		        
+		        int greatest;
+
+		        if (num1 >= num2 && num1 >= num3) {
+		            greatest = num1;
+		        } else if (num2 >= num1 && num2 >= num3) {
+		            greatest = num2;
+		        } else {
+		            greatest = num3;
+		        }
+
+		       
+		        System.out.println("The greatest number is: " + greatest);
+
+	}
+
+
+
+
+    }
+
+
